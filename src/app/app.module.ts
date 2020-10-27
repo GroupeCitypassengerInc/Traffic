a31b3f03cf3928ad84370be2e0eadbe3d59d7663
RodolpheGhio <rodolpheghio@gmail.com>
2020-10-27 13:13:57 +0100

    added map, added map marker from JSON, fixed css and made it a bit responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/map/map.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import * as groups from './json/map_devices.json';
+
+declare const L:any;
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+
+export class MapComponent implements OnInit {
+  
+  title = 'Citymap';
+  markers: marker[];
+  
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    let mymap = L.map('map').setView([51.505, -0.09], 2);
+    
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=sk.eyJ1Ijoicm9kb2xwaGVnaGlvIiwiYSI6ImNrZ3IxeHZrMzA5dm8zMG83eWx3bm45cnMifQ.hANaQ5_PG5Y5AExWWIljfQ', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+    }).addTo(mymap);
+
+    for (let group of groups.groups){
+      for (let sites of group.sites){
+        let lat = sites.datas.mapInfo.latitude;
+        let long = sites.datas.mapInfo.longitude;
+        let marker = L.marker([lat, long], {title:sites.siteName.toString()}).addTo(mymap);
+        let pop_up_info ='Group: ' + group.displayName.toString() + '<br>'+'Name: ' + sites.siteName.toString() + '<br>' + 'Address: ' + sites.datas.address.toString();
+        marker.bindPopup(pop_up_info);
+        marker.on('click', onClick);
+
+        let container = L.DomUtil.create ('div');
+      }
+    }
+
+    function onClick(e) {
+      var popup = e.target.getPopup();
+      var content = popup.getContent();
+      console.log(content);
+    }
+  }
+
+  initialize_map_markers(){
+    console.log(groups.groups);
+  }
+}
+
+interface marker {
+	lat: number;
+	lng: number;
+	label?: string;
+	draggable: boolean;
+}
