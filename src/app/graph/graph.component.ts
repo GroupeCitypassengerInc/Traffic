9b1fbf9d355fbfb3593572b373fe7615d07bc06c
RodolpheGhio <rodolpheghio@gmail.com>
2020-12-11 08:50:25 -0500

    time select ok

--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input, SimpleChanges, ChangeDetectorRef, Application
 import { HttpClientModule, HttpClient, HttpHeaders }    from '@angular/common/http';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule } from '@angular/material/core';
@@ -11,9 +11,11 @@ import { NgModule, LOCALE_ID } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDatetimepickerModule, MatNativeDatetimeModule } from '@mat-datetimepicker/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { Chart } from 'chart.js';
 import * as ChartDatasourcePrometheusPlugin from 'chartjs-plugin-datasource-prometheus';
+import { throwError } from 'rxjs';
 
 export interface Graph_records {
   [ metric_name : string ] : {
@@ -21,7 +23,14 @@ export interface Graph_records {
     t_value : number,
     t_unit : 'minute' | 'hour' | 'day',
     t_date : any, // Have to change this
+    t_now : boolean,
   }
+}
+
+export interface unit_conversion {
+  minute : number,
+  hour : number,
+  day : number,
 }
 
 @Component({
@@ -35,33 +44,41 @@ export class GraphComponent implements OnInit {
   @Input() information: Array<string>;
   Object = Object;
 
+  query_list : any = [];
+
   // Request : /prometheus/api/v1/query_range?query=up&start=1604584181.313&end=1604670581.313&step=9250
   endpoint : string = 'http://10.0.0.68:12333/prometheus/';
-  query_list : any = [];
-  up_start_time : number = -1 * 60 * 60 * 1000; //1 hours from now (numbre of ms)
-  end_time : number = 0; //(numbre of ms) now
+
+  default_up_start_time : number = -12 * 60 * 60 * 1000;
+  default_end_time : any = 0;
+  up_start_time : number = this.default_up_start_time;
+  end_time : number = this.default_end_time;
   start_date : Date;
+
   form_group: FormGroup;
   graphs_records : any = {};
   default_value: number = 1;
   default_date: Date = new Date();
+
   _step: number = 1;
   _min: number = 0;
   _max: number = Infinity;
   _wrap: boolean = true;
+  _now: boolean = true;
   color: string = 'primary';
-
   default_unit: 'minute' | 'hour' | 'day' = 'hour';
+  _unit : unit_conversion = {
+    minute : 60 * 1000,
+    hour : 60 * 60 * 1000,
+    day : 27 * 60 * 60 * 1000,
+  }
   unit_select = new FormControl(false);
 
   constructor(private appRef: ChangeDetectorRef,  private _formBuilder: FormBuilder) {
     this.form_group = this._formBuilder.group({
       default_date: [{ value: '', disabled: true }, Validators.required]
     });
-    this.form_group.valueChanges.subscribe(x => {
-      console.log(x)
-      this.date_changes(x.keys);
-    });
+    
   }
   
   ngOnInit(): void {
@@ -69,6 +86,9 @@ export class GraphComponent implements OnInit {
     this.default_date.setHours(this.default_date.getHours());
     this.query_list = this.information;
     this.get_records();
+    this.form_group.valueChanges.subscribe(date => {
+      this.date_changes(date);
+    });
   }
 
   ngAfterViewInit(): void {
@@ -98,6 +118,7 @@ export class GraphComponent implements OnInit {
           t_date : this._formBuilder.control({
             value: this.default_date, disabled: false
           }),
+          t_now : this._now,
         }
         this.form_group.addControl(query, this.graphs_records[query]['t_date']);
       }
@@ -133,16 +154,12 @@ export class GraphComponent implements OnInit {
     this.generate_all_graph();
   }
 
-  // Re-generate all graph
+  // Re-generate graph
   regenerate(id:string): void {
-    console.log('regenerating ' + id + ' chart');
-    let chart = this.graphs_records[id]['m_chart'];
-
-    console.log ('destroying ' + id + ' chart');
-    chart.destroy();
-
-    console.log ('re-building ' + id + ' chart');
-    this.graphs_records[id]['m_chart'] = chart;
+    //console.log ('destroying ' + id + ' chart');
+    this.graphs_records[id]['m_chart'].destroy();
+    //console.log ('re-building ' + id + ' chart');
+    this.graphs_records[id]['m_chart'] = this.chart_builder(id);
   }
   
   // Build chart
@@ -191,13 +208,13 @@ export class GraphComponent implements OnInit {
     return chart;
   }
 
-  incrementValue(step: number = 1, metric : string): void {
-    let inputValue = this.graphs_records[metric]['t_value'] + step;
+  incrementValue(step: number = 1, query : string): void {
+    let inputValue = this.graphs_records[query]['t_value'] + step;
     if ( this._wrap ) {
       inputValue = this.wrappedValue(inputValue);
     }
-    this.graphs_records[metric]['t_value'] = inputValue;
-    console.log(this.graphs_records[metric]);
+    this.graphs_records[query]['t_value'] = inputValue;
+    this.time_value_changes(query);
   }
 
   setColor(color: string): void {
@@ -228,19 +245,50 @@ export class GraphComponent implements OnInit {
   }
 
   unit_selection_changes(query: string){
-    console.log (this.graphs_records[query]['t_unit']+' '+query);
+    let t_value = this.graphs_records[query]['t_value'];
+    let t_unit = this.graphs_records[query]['t_unit'];
+    this.up_start_time = -1 * t_value * this._unit[t_unit] + this.end_time;
+    this.regenerate(query);
   }
 
   time_value_changes(query: string){
-    console.log (this.graphs_records[query]['t_value']+' '+query);
+    let t_value = this.graphs_records[query]['t_value'];
+    let t_unit = this.graphs_records[query]['t_unit'];
+    this.up_start_time = -1 * t_value * this._unit[t_unit] + this.end_time;
+    this.regenerate(query);
   }
 
   date_changes(date){
-    console.log(date);
+    let query = Object.keys(date).toString();
+    this.default_date = new Date();
+    let current_timestamp = this.default_date.getTime();
+    let selected_date_timestamp = date[query].getTime();
+    this.end_time = (current_timestamp - selected_date_timestamp) * -1;
+    let t_value = this.graphs_records[query]['t_value'];
+    let t_unit = this.graphs_records[query]['t_unit'];
+    this.up_start_time = -1 * t_value * this._unit[t_unit] + this.end_time;
+    this.regenerate(query);
+    if ( this.end_time < this.up_start_time ) {
+      this.end_time = 0;
+      throw new Error('the selected date must not be in the future. Selected date : ' + date[query].toTimeString() );
+    } else {
+      this.regenerate(query);
+    }
   }
 
-  change_chart_timerange(){
-    this.end_time;
-    this.up_start_time;
+  on_checkbox_change($event:Event, query:string){
+    if ( $event['checked'] ) {
+      let t_value = this.graphs_records[query]['t_value'];
+      let t_unit = this.graphs_records[query]['t_unit'];
+      this.up_start_time = -1 * t_value * this._unit[t_unit] + this.end_time;
+      this.end_time = 0;
+      this.regenerate(query);
+    }
+  }
+
+  set_default_settings(query:string){
+    this.up_start_time = this.default_up_start_time;
+    this.end_time = this.default_end_time;
+    this.regenerate(query);
   }
 }
