9911d311387191f8420ad8aeafdfef4c1245f94b
RodolpheGhio <rodolpheghio@gmail.com>
2020-12-17 11:40:36 -0500

    prepared routing, login/logout testable, graph selection testable (working with a json file) and graph display testable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   appVersion: require('../../package.json').version,
   base_url:'https://current.citypassenger.com:4443/prometheus/',
-  city_url_api: 'https://preprod.citypassenger.com/'
+  city_url_api: 'https://preprod.citypassenger.com'
 };
