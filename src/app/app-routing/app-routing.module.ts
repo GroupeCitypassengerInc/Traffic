4cf43ad1bea34b21015be762c458f9b2a7ab9c40
RodolpheGhio <rodolpheghio@gmail.com>
2020-12-21 11:46:15 -0500

    redirect to login for "" and "**"

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -9,8 +9,8 @@ import { LoginComponent } from '../login/login.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'graph', component: DevicesTableComponent },
-  { path: '', component: LoginComponent },  // Wildcard route for a 404 page
-  { path: '**', component: LoginComponent },  // Wildcard route for a 404 page
+  { path: '', redirectTo: '/login', pathMatch: 'full' },  // Wildcard route for a 404 page
+  { path: '**', redirectTo: '/login', pathMatch: 'full' },  // Wildcard route for a 404 page
 
 ]; // sets up routes constant where you define your routes
 
