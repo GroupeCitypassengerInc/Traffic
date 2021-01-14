548217e116a6faaee2ce6546e984211f9ae28854
RodolpheGhio <rodolpheghio@gmail.com>
2021-01-14 09:35:38 -0500

    notification service now support multilingual tittle

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBar, MAT_SNACK_BAR_DATA, MatSnackBarRef } from '@angular/material/snack-bar';
+import { LanguageService } from '../lingual_service/language.service';
 
 @Component({
   selector: 'app-notification',
@@ -8,10 +9,13 @@ import { MatSnackBar, MAT_SNACK_BAR_DATA, MatSnackBarRef } from '@angular/materi
   styleUrls: ['./notification.component.css']
 })
 export class NotificationComponent implements OnInit {
-
-  constructor(@Inject(MAT_SNACK_BAR_DATA) public data:any, public snackBarRef:MatSnackBarRef<NotificationComponent>) { }
+  _lang: string;
+  constructor(@Inject(MAT_SNACK_BAR_DATA) public data:any, public snackBarRef:MatSnackBarRef<NotificationComponent>, private lang: LanguageService) { }
 
   ngOnInit(): void {
+    this._lang = this.lang.get_language();
+    if ( this._lang == 'fr' ){
+      this.data.type = this.data.type === 'error' ? 'Erreur' : 'Succès';
+    }
   }
-
 }
